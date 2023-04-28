8ee9f75f606f941d936286827d54ec41271ca11a
Dorra <dorra.ayari@esprit.tn>
2023-04-28 18:02:08 +0100

    new

--- app/Http/Controllers/JWTController.php
+++ app/Http/Controllers/JWTController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Support\Facades\Cookie;
 
 class JWTController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -40,129 +40,8 @@ use App\Http\Controllers\TicketAttachementsController;
 Route::middleware(['jwt.verify'])->group(function() {
     Route::group(['middleware'=>'api'],function(){
 
-        ///Ticket
-    Route::get('Tickets/{id}/show', [TicketController::class,'show']);
-    Route::get('Tickets', [TicketController::class,'index']);
-    Route::get('priority', [TicketController::class,'priority']);
 
-    Route::delete('Tickets/{id}/delete', [TicketController::class,'destroy']);
-    Route::put('Tickets/{id}/update', [TicketController::class,'update']);
-    Route::put('TicketsClient/{id}/update', [TicketController::class,'updateclient']);
-    Route::put('TicketsPick/{id}/update', [TicketController::class,'updatepick']);
-    Route::post('Tickets/create',[TicketController::class,'store']);
 
-///Category:
-Route::get('Category/{id}/show', [CategoryController::class,'show']);
-    Route::get('Category', [CategoryController::class,'index']);
-    Route::delete('Category/{id}/delete', [CategoryController::class,'destroy']);
-    Route::put('Category/{id}/update', [CategoryController::class,'update']);
-    Route::post('Category/create',[CategoryController::class,'store']);
-    ///////
-///Status:
-Route::get('Status/{id}/show', [StatusController::class,'show']);
-    Route::get('Status', [StatusController::class,'index']);
-    Route::delete('Status/{id}/delete', [StatusController::class,'destroy']);
-    Route::put('Status/{id}/update', [StatusController::class,'update']);
-    Route::post('Status/create',[StatusController::class,'store']);
-    ///////
-///Levels:
-Route::get('Levels/{id}/show', [LevelsController::class,'show']);
-    Route::get('Levels', [LevelsController::class,'index']);
-    Route::delete('Levels/delete/{id}', [LevelsController::class,'destroy']);
-    Route::put('Levels/{id}/update', [LevelsController::class,'update']);
-    Route::post('Levels/create',[LevelsController::class,'store']);
-    ///////
-///Impact:
-Route::get('Impact/{id}/show', [ImpactController::class,'show']);
-    Route::get('Impact', [ImpactController::class,'index']);
-    Route::delete('Impact/{id}/delete', [ImpactController::class,'destroy']);
-    Route::put('Impact/{id}/update', [ImpactController::class,'update']);
-    Route::post('Impact/create',[ImpactController::class,'store']);
-    ///////
-///Departements:
-Route::get('Departements/{id}/show', [DepartementsController::class,'show']);
-    Route::get('Departements', [DepartementsController::class,'index']);
-    Route::delete('Departements/{id}/delete', [DepartementsController::class,'destroy']);
-    Route::put('Departements/{id}/update', [DepartementsController::class,'update']);
-    Route::post('Departements/create',[DepartementsController::class,'store']);
-    ///////
-///Urgency:
-Route::get('Urgency/{id}/show', [UrgencyController::class,'show']);
-    Route::get('Urgency', [UrgencyController::class,'index']);
-    Route::delete('Urgency/{id}/delete', [UrgencyController::class,'destroy']);
-    Route::put('Urgency/{id}/update', [UrgencyController::class,'update']);
-    Route::post('Urgency/create',[UrgencyController::class,'store']);
-    ///////
-///Priority:
-Route::get('Priority/{id}/show', [PriorityController::class,'show']);
-Route::get('Priority', [PriorityController::class,'index']);
-Route::delete('Priority/{id}/delete', [PriorityController::class,'destroy']);
-Route::put('Priority/{id}/update', [PriorityController::class,'update']);
-Route::post('Priority/create',[PriorityController::class,'store']);
-///////
-///RequestType:
-Route::get('RequestType/{id}/show', [RequestTypeController::class,'show']);
-Route::get('RequestType', [RequestTypeController::class,'index']);
-Route::delete('RequestType/{id}/delete', [RequestTypeController::class,'destroy']);
-Route::put('RequestType/{id}/update', [RequestTypeController::class,'update']);
-Route::post('RequestType/create',[RequestTypeController::class,'store']);
-///////
-///Category:
-Route::get('Category/{id}/show', [CategoryController::class,'show']);
-Route::get('Category', [CategoryController::class,'index']);
-Route::delete('Category/{id}/delete', [CategoryController::class,'destroy']);
-Route::put('Category/{id}/update', [CategoryController::class,'update']);
-Route::post('Category/create',[CategoryController::class,'store']);
-///////
-///TicketModel:
-Route::get('TicketModel/{id}/show', [TicketModelController::class,'show']);
-Route::get('TicketModel', [TicketModelController::class,'index']);
-Route::delete('TicketModel/{id}/delete', [TicketModelController::class,'destroy']);
-Route::put('TicketModel/{id}/update', [TicketModelController::class,'update']);
-Route::post('TicketModel/create',[TicketModelController::class,'store']);
-///////
-///Locations:
-Route::get('Locations/{id}/show', [LocationsController::class,'show']);
-Route::get('Locations', [LocationsController::class,'index']);
-Route::delete('Locations/{id}/delete', [LocationsController::class,'destroy']);
-Route::put('Locations/{id}/update', [LocationsController::class,'update']);
-Route::post('Locations/create',[LocationsController::class,'store']);
-///////
-///TicketCloseModel:
-Route::get('TicketCloseModel/{id}/show', [TicketCloseModelController::class,'show']);
-Route::get('TicketCloseModel', [TicketCloseModelController::class,'index']);
-Route::delete('TicketCloseModel/{id}/delete', [TicketCloseModelController::class,'destroy']);
-Route::put('TicketCloseModel/{id}/update', [TicketCloseModelController::class,'update']);
-Route::post('TicketCloseModel/create',[TicketCloseModelController::class,'store']);
-///////
-///Comments:
-Route::get('Comments/{id}/show', [CommentsController::class,'show']);
-Route::get('Comments', [CommentsController::class,'index']);
-Route::delete('Comments/{id}/delete', [CommentsController::class,'destroy']);
-Route::put('Comments/{id}/update', [CommentsController::class,'update']);
-Route::post('Comments/create',[CommentsController::class,'store']);
-///////
-///SubCategory:
-Route::get('SubCategory/{id}/show', [SubCategoryController::class,'show']);
-Route::get('SubCategory', [SubCategoryController::class,'index']);
-Route::delete('SubCategory/{id}/delete', [SubCategoryController::class,'destroy']);
-Route::put('SubCategory/{id}/update', [SubCategoryController::class,'update']);
-Route::post('SubCategory/create',[SubCategoryController::class,'store']);
-///////
-///Items:
-Route::get('Items/{id}/show', [ItemsController::class,'show']);
-Route::get('Items', [ItemsController::class,'index']);
-Route::delete('Items/{id}/delete', [ItemsController::class,'destroy']);
-Route::put('Items/{id}/update', [ItemsController::class,'update']);
-Route::post('Items/create',[ItemsController::class,'store']);
-///////
-///TicketAttachements:
-Route::get('TicketAttachements/{id}/show', [TicketAttachementsController::class,'show']);
-Route::get('TicketAttachements', [TicketAttachementsController::class,'index']);
-Route::delete('TicketAttachements/{id}/delete', [TicketAttachementsController::class,'destroy']);
-Route::put('TicketAttachements/{id}/update', [TicketAttachementsController::class,'update']);
-Route::post('TicketAttachements/create',[TicketAttachementsController::class,'store']);
-///////
 ///RolePermission:
 Route::get('RolePermission/{id}/show', [RolePermissionController::class,'show']);
 Route::get('RolePermission', [RolePermissionController::class,'index']);
@@ -170,13 +49,7 @@ Route::delete('RolePermission/{id}/delete', [RolePermissionController::class,'de
 Route::put('RolePermission/{id}/update', [RolePermissionController::class,'update']);
 Route::post('RolePermission/create',[RolePermissionController::class,'store']);
 ///////
-///CategoryMembers:
-Route::get('CategoryMembers/{id}/show', [CategoryMembersController::class,'show']);
-Route::get('CategoryMembers', [CategoryMembersController::class,'index']);
-Route::delete('CategoryMembers/{id}/delete', [CategoryMembersController::class,'destroy']);
-Route::put('CategoryMembers/{id}/update', [CategoryMembersController::class,'update']);
-Route::post('CategoryMembers/create',[CategoryMembersController::class,'store']);
-///////
+
 ///User:
 Route::get('User/{id}/show', [UserController::class,'show']);
 Route::get('User', [UserController::class,'index']);
@@ -199,7 +72,7 @@ Route::post('/imageProfil', [UserController::class, 'uploadimage']);
 // Search
 Route::get('/search', [SearchController::class, 'search']);
 // Messages
-Route::post('messages', [ChatController::class, 'message']);
+
 
 
 
@@ -210,7 +83,7 @@ Route::post('refresh', [JWTController::class,'refresh']);
     Route::post('me', [JWTController::class,'me']);
 });
 });
-
+Route::post('messages', [ChatController::class, 'message']);
 ///// JWT auth
 Route::post('login', [JWTController::class,'login']);
 Route::post('register', [JWTController::class,'register']);
